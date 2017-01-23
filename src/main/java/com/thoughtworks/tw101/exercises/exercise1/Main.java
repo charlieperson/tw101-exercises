38d40260593e43b5178f985428ec35711f36b8aa
Charlie Person <charlie.person@hyperisland.se>
2017-01-23 14:27:11 -0600

    Completes exercise one and modifies .gitignore to ignore build directory

--- src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -4,6 +4,20 @@ package com.thoughtworks.tw101.exercises.exercise1;
 
 public class Main {
     public static void main(String[] args) {
+        printOddsAndSumUp(100);
+    }
 
+    public static void printOddsAndSumUp(int high) {
+        int sum   = 0;
+        int count = 0;
+
+        while(count <= high) {
+            if(count % 2 == 1) {
+                System.out.println("Count: " + count);
+                sum += count;
+            }
+            count++;
+        }
+        System.out.println("Sum = " + sum);
     }
 }
