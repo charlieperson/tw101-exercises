dc2916d5a25628a4953d486ace148adf26c905b1
Charlie Person <charlie.person@hyperisland.se>
2017-01-23 16:39:08 -0600

    Completes fizzBuzz exercise

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/FizzBuzz.java
@@ -28,10 +28,23 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        fizzBuzz();
+        fizzBuzz(100);
     }
 
-    private static void fizzBuzz() {
+    private static void fizzBuzz(int high) {
+        int count = 1;
 
+        while(count <= high) {
+            if(count % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if(count % 5 == 0) {
+                System.out.println("Buzz");
+            } else if(count % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(count);
+            }
+            count++;
+        }
     }
 }
