16340351bf772837592de6a616503b9d29a8f41a
Charlie Person <charlie.person@hyperisland.se>
2017-02-06 22:02:59 +0000

    Completes exercise 5

--- src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
@@ -3,6 +3,9 @@ package com.thoughtworks.tw101.exercises.exercise5;
 public class RectangleAverager {
 
     public float averageArea(Rectangle[] rectangles) {
-        return 0;
+        int numberOfRectangles = rectangles.length;
+        int totalArea = 0;
+        for(Rectangle rect: rectangles) { totalArea += rect.area(); }
+        return totalArea / numberOfRectangles;
     }
 }
