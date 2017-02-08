f50cb7f8a72a2ece7c3062f063e3adb042cc4d65
Charlie Person <charlie.person@hyperisland.se>
2017-02-08 16:48:42 +0000

    Outputting names in alphabetical order but not yet building up list and needs to be refactored

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -1,12 +1,15 @@
 package com.thoughtworks.tw101.exercises.exercise9;
 
-import javax.xml.soap.SAAJResult;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
     private String name;
-    private Node laterChildNode;
-    private Node earlierChildNode;
+    private Node right;
+    private Node left;
+    private Node parent;
+    private boolean checked = false;
+    private ArrayList<String> orderedNames = new ArrayList<>();
 
     public Node(String name) {
         this.name = name;
@@ -22,18 +25,20 @@ public class Node {
     }
 
     private void addLaterChild(String nameOfNewNode) {
-        if(laterChildNode == null) {
-            laterChildNode = new Node(nameOfNewNode);
+        if(right == null) {
+            right = new Node(nameOfNewNode);
+            right.setParent(this);
         } else {
-            laterChildNode.add(nameOfNewNode);
+            right.add(nameOfNewNode);
         }
     }
 
     private void addEarlierChild(String nameOfNewNode) {
-        if(earlierChildNode == null) {
-            earlierChildNode = new Node(nameOfNewNode);
+        if(left == null) {
+            left = new Node(nameOfNewNode);
+            left.setParent(this);
         } else {
-            earlierChildNode.add(nameOfNewNode);
+            left.add(nameOfNewNode);
         }
     }
 
@@ -44,19 +49,51 @@ public class Node {
         return false;
     }
 
+//    If node has a node to the left, call the function on that sub node
+//    If the node does not have a node to the left, output the node's name and mark it as checked
+//    If the node has a node to the right, run the function on that node
+//    If the node does not have a node to the right or left, run the function on the node's parent
+//    If the node has been checked, run the function on it's parent.
+//    If the node does not have a parent, we know it's the root
+
+
     public List<String> names() {
-        return null;
+        if(parent == null && left.checked) {
+            if(right.checked) return null;
+            System.out.println(getName());
+            checked = true;
+            if(right != null && !right.checked) {
+                right.names();
+            }
+        }
+        if(checked) return parent.names();
+        if(left != null && !left.checked) {
+            left.names();
+        } else if(right != null && !right.checked) {
+            System.out.println(getName());
+            checked = true;
+            right.names();
+        } else {
+            System.out.println(getName());
+            checked = true;
+            parent.names();
+        }
+        return orderedNames;
     }
 
     public Node getLaterChildNode() {
-        return laterChildNode;
+        return right;
     }
 
     public Node getEarlierChildNode() {
-        return earlierChildNode;
+        return left;
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
     }
 }
