8a48fdb90bba817b69fa4536e5668c8c2dc9d897
Charlie Person <charlie.person@hyperisland.se>
2017-02-13 22:14:32 -0800

    Makes all methods in SecretNumberGenerator non-static

--- src/main/java/com/thoughtworks/tw101/exercises/exercise8/SecretNumberGenerator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise8/SecretNumberGenerator.java
@@ -3,13 +3,13 @@ package com.thoughtworks.tw101.exercises.exercise8;
 import java.util.Random;
 
 public class SecretNumberGenerator {
-    private static int number = generate();
+    private int number = generate();
 
     public int getNumber() {
         return number;
     }
 
-    private static int generate() {
+    private int generate() {
         Random randomGenerator = new Random();
         int randomInt = randomGenerator.nextInt(100);
         return randomInt;
